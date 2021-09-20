8960468abbdc22ea76dbf9ca1f2740190e798c73
chuliang014 <chuliang014@gmail.com>
2021-09-19 17:30:27 -0700

    feat: add useMount and useDebounce two hooks

--- jira/src/screens/project-list/index.jsx
+++ jira/src/screens/project-list/index.jsx
@@ -3,32 +3,36 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { cleanObject } from "utils";
 import qs from "qs";
+import { useMount, useDebounce } from "utils";
 
 const apiURL = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
-  const [list, setList] = useState([]);
+
   const [param, setParam] = useState({
     name: "",
     personId: ""
   });
-
-  useEffect(() => {
-    fetch(`${apiURL}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([]);
+  useEffect(() => { 
+    fetch(
+      `${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async response => {
       if (response.ok) {
         setList(await response.json());
       }
     });
-  }, [param]);
-  
-  useEffect(() => {
+  }, [debouncedParam]);
+
+  useMount(() => {
     fetch(`${apiURL}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
