1f89c92b04810ef5d7bfb98dc77675a4331034aa
chuliang014 <chuliang014@gmail.com>
2021-09-20 22:06:43 -0700

    chore: assignment

--- jira/src/utils/index.ts
+++ jira/src/utils/index.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 //在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object:object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach(key => {
     //@ts-ignore
@@ -16,7 +16,7 @@ export const cleanObject = (object:object) => {
 };
 
 // hook要以use开头
-export const useMount = (callback: ()=> void)=> {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
@@ -25,13 +25,31 @@ export const useMount = (callback: ()=> void)=> {
 // 后面用泛型来规范类型
 export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
-  
+
   useEffect(() => {
     // 每次在value变化以后，设置一个定时器
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
-    // 每次在上一个useEffect处理完以后再运行 
+    // 每次在上一个useEffect处理完以后再运行
     return () => clearTimeout(timeout);
   }, [value, delay]);
 
-  return debouncedValue
+  return debouncedValue;
+};
+
+export const useArray = <T>(entity: T[]) => {
+  const [value, setValue] = useState(entity);
+  const clear = () => {
+    setValue([]);
+  };
+  const removeIndex = (index: number) => {
+    const temp = [...value];
+    temp.splice(index, 1);
+    setValue(temp);
+  };
+
+  const add = (item: T) => {
+    setValue([...value, item]);
+  };
+
+  return { value, clear, removeIndex, add };
 };
