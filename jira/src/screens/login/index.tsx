e73811308083e460a87812902b78a57cfa8444dc
chuliang014 <chuliang014@gmail.com>
2021-09-22 22:12:20 -0700

    chore: auth-provider

--- jira/src/screens/login/index.tsx
+++ jira/src/screens/login/index.tsx
@@ -1,27 +1,14 @@
 import React, { FormEvent } from "react";
 
-const apiURL = process.env.REACT_APP_API_URL;
-
 export const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiURL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(param)
-    }).then(async response => {
-      if (response.ok) {
-      }
-    });
-  };
+  const login = (param: { username: string; password: string }) => {};
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const username = (event.currentTarget.elements[0] as HTMLInputElement)
       .value;
     const password = (event.currentTarget.elements[1] as HTMLInputElement)
       .value;
-      login({username, password})
+    login({ username, password });
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -29,7 +16,7 @@ export const LoginScreen = () => {
         <label htmlFor="username">用户名</label>
         <input type="text" id={"username"} />
       </div>
-      <div>
+      <div> 
         <label htmlFor="password">密码</label>
         <input type="password" id={"password"} />
       </div>
